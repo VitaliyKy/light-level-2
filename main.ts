1c706b511a634444ed7242442e24a9f76c60a679
VitaliyKy <130439293+VitaliyKy@users.noreply.github.com>
2023-04-11 09:10:00 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,27 @@
+basic.forever(function () {
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # # # # #
+            . . # . .
+            . . # . .
+            `)
+    } else if (input.lightLevel() > 100 && input.lightLevel() < 150) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . # . # .
+            . . # . .
+            . # . # .
+            # . . . #
+            `)
+    }
+})
